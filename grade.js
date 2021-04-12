57c4014773c1a052563ecbeddcc891981ba0b97e
Rafael Pauwels <rafapauwels@gmail.com>
2021-04-12 17:51:14 -0300

    JS

--- grade.js
+++ grade.js
@@ -17,30 +17,46 @@ function loadCards() {
     const dados = JSON.parse(localStorage.getItem('aulasData'))
     console.log(dados)
 
-    for (var i = 0; i < dados.length; i++) {
-        var prop = dados[i]
+    document.getElementById("ra-aluno").innerText = dados.ra
+    for (var i = 0; i < dados.turmas.length; i++) {
+        var prop = dados.turmas[i]
         var element = document.createElement("div")
-        element.innerHTML = `<div class="p-3 class-block force-bottom">
-            <div class="class-icon"><i class="bi bi-journal-bookmark text-indigo"></i></div>
-            <div class="class-header">
-              <h3 class="class-name text-indigo fs-5">${prop.nome_materia}</h3>
-              <div class="class-teacher text-gray fs-6"><i class="bi bi-person"></i>${prop.docente_teoria}</div>
-            </div>
-            <ul class="list-group">
-              <li class="list-group-item"><span class="badge bg-indigo">QUA</span> <span class="badge bg-secondary">19h00
-                  - 21h00</span> <span class="badge bg-indigo">SI</span></li>
-              <li class="list-group-item"><span class="badge bg-indigo">QUA</span> <span class="badge bg-secondary">21h00
-                  - 23h00</span> <span class="badge bg-indigo">SII</span></li>
-              <li class="list-group-item"><span class="badge bg-indigo">SEX</span> <span class="badge bg-secondary">19h00
-                  - 21h00</span></li>
-            </ul>
-            <div class="class-body bottom-item">
-              <div class="container-fix">
-                <div class="col-12"><span class="rounded-text bg-gray text-dark">Sala 107</span> <span
-                    class="rounded-text bg-gray text-dark">Santo André</span></div>
-              </div>
-            </div>
-          </div>`
+        element.innerHTML = `
+        <div class="p-3 class-block force-bottom">
+          <div class="class-icon"><i class="bi bi-journal-bookmark text-indigo"></i></div>
+          <div class="class-header">
+            <!--nome da matéria-->
+            <h3 class="class-name text-indigo fs-5">${prop.nome_materia}</h3>
+            <!--docentes-->
+            <div class="class-teacher text-gray fs-6"><i class="bi bi-person-plus"></i> ${prop.docente_teoria}</div>
+            <div class="class-teacher text-gray fs-6"><i class="bi bi-person-dash"></i> ${prop.docente_pratica}</div>
+          </div>
+          <!--lista de aulas na semana-->
+          <ul class="list-group">
+            <!--item de aula 1 da semana-->
+            ${prop.dia1 != "" ? `<li class="list-group-item">
+                <span class="badge bg-indigo">${prop.dia1}</span>
+                <span class="badge bg-secondary">${prop.horario1}</span>
+                <span class="badge bg-indigo">${prop.rotacao1}</span>
+            </li>` : ""}
+            ${prop.dia2 != "" ? `<li class="list-group-item">
+                <span class="badge bg-indigo">${prop.dia2}</span>
+                <span class="badge bg-secondary">${prop.horario2}</span>
+                <span class="badge bg-indigo">${prop.rotacao2}</span>
+            </li>` : ""}
+            ${prop.dia3 != "" ? `<li class="list-group-item">
+                <span class="badge bg-indigo">${prop.dia3}</span>
+                <span class="badge bg-secondary">${prop.horario3}</span>
+                <span class="badge bg-indigo">${prop.rotacao3}</span>
+            </li>` : ""}
+            ${prop.dia4 != "" ? `<li class="list-group-item">
+                <span class="badge bg-indigo">${prop.dia4}</span>
+                <span class="badge bg-secondary">${prop.horario4}</span>
+                <span class="badge bg-indigo">${prop.rotacao4}</span>
+            </li>` : ""}
+          </ul>
+        </div>
+      </div>`
         element.classList.add("col-12")
         element.classList.add("col-sm-6")
         element.classList.add("col-lg-4")
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode == 13) {
+        event.preventDefault()
+        busca()
+    }
+})
+
 async function busca() {
     var ra = document.getElementById("ra-input").value
-    await fetch("http://127.0.0.1:8080/alunos/" + ra)
+    await fetch("https://18.228.138.74/alunos/" + ra)
         .then(data => data.json().then(json => {
             console.log(JSON.stringify(json))
-            localStorage.setItem('aulasData', JSON.stringify(json))
-            document.location.pathname = '/minha-grade.html'
+            if (json.turmas.length != 0) {
+                localStorage.setItem('aulasData', JSON.stringify(json))
+                document.location.pathname = '/minha-grade.html'
+            } else {
+                console.log("RA não encontrado")
+            }
         }))
 }
